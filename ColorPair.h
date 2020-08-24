dadb545425c7f11cb22c796302855a6ec8779a14
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-24 19:34:23 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -3,8 +3,8 @@
 
 namespace TelCoColorCoder
 {
-    enum class MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum class MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
     const char* MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
