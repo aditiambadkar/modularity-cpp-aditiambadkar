ed9aabf1a497b8ed15f03008ff7bc40bb2c4694b
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-25 10:58:49 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -4,7 +4,6 @@
 
 using namespace TelCoColorCoder;
 
-    MajorColor = {WHITE, RED, BLACK, YELLOW, VIOLET};
     ColorPair::ColorPair(MajorColor major, MinorColor minor) : 
         majorColor(major), minorColor(minor) {};
     MajorColor ColorPair::getMajor() {
