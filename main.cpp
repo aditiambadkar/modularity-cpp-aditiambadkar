c9a05ab3890078fcaa00d3ffd0990b18cab2483d
aditiambadkar <54082235+aditiambadkar@users.noreply.github.com>
2020-08-25 11:10:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor)
 {
     TelCoColorCoder::ColorPair colorPair =
-        GetColorFromPairNumber(pairNumber);
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert(colorPair.getMajor() == expectedMajor);
     assert(colorPair.getMinor() == expectedMinor);
